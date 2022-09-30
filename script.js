348d1ee09493aed56d62f6ec06cfe6238f465294
lucas19salles <lucassalles19@hotmail.com>
2022-09-30 16:46:50 -0300

    Controle Financeiro

---
+++ script.js
@@ -0,0 +1,22 @@
+let saldo = prompt("Informe a quantidade de dinheiro inicial: ");
+saldo = parseFloat(saldo);
+let opcao = "";
+
+do {
+  opcao = prompt(
+    `Saldo disponível: R$ ${saldo} \n1- Depósitar \n2- Saque \n3- Encerrar`
+  );
+  switch (opcao) {
+    case "1":
+      saldo += parseFloat(prompt("Qual valor deseja depósitar?"));
+      break;
+
+    case "2":
+      saldo -= parseFloat(prompt("Qual valor deseja sacar?"));
+      break;
+
+    case "3":
+      alert(`Encerrando`);
+      break;
+  }
+} while (opcao !== "3");
